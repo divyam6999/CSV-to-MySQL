633d839c9b96de99abce6cd94056c920b8a6ca0f
Baukhandi <Divyam.Baukhandi@bottomline.com>
2023-03-09 14:20:25 +0530

    updated item processor

--- src/main/java/com/batch/config/Data_Processor.java
+++ src/main/java/com/batch/config/Data_Processor.java
@@ -1,4 +1,5 @@
 package com.batch.config;
+import org.apache.tomcat.jni.User;
 import org.springframework.batch.item.ItemProcessor;
 
 import com.batch.entities.Data;
@@ -6,7 +7,17 @@ import com.batch.entities.Data;
 
 public class Data_Processor implements ItemProcessor<Data, Data>{
     @Override
-    public Data process(Data data_init) throws Exception {
-        return data_init;
+    public Data process(Data data) throws Exception {
+        return new Data(
+        		data.getDate_reported(),
+        		data.getCountry_code().toLowerCase(),
+        		data.getCountry().toUpperCase(),
+        		data.getWHO_region().toLowerCase(),
+        		data.getNew_cases(),
+        		data.getCumulative_cases(),
+        		data.getNew_deaths(),
+        		data.getCumulative_deaths()
+        		
+        		);
     }
 }
